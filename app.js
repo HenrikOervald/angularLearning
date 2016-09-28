ee006e389381e82eb6ab47a1b2d5ecf158e20625
HenrikOervald <henrik.oervald@gmail.com>
2016-09-28 18:05:14 +0200

    trying to set views at app

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 app.use(express.static(__dirname + '/public'));
 
-
-
-
+app.set('views', (__dirname+'/views'));
 app.get('/', function (req, res) {
   res.render('pages/index');
 });
