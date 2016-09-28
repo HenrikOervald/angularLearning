e1bf0b921a13d11e322a259c8fcb1b6131326f60
HenrikOervald <henrik.oervald@gmail.com>
2016-09-28 16:26:07 +0200

    public now holds static files.. might need an update

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 8080;
-app.set('view engine', 'html');
+var port = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function (req, res) {
   res.render('index');
-
-res.status(200);
 });
 
-app.listen(port, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port '+ port +'!');
 });
