3d1a11c92d7b9d4bd926ccc6e4b26c7bf9f22a93
HenrikOervald <henrik.oervald@gmail.com>
2016-09-29 11:12:15 +0200

    aerg

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function (req, res) {
-  res.sendFile('./views/index');
+  res.sendFile('./views/index.html');
 });
 
 app.listen(PORT, function () {
