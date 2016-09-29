1edd0e9858746e910d482fd05cf392911e1c4242
HenrikOervald <henrik.oervald@gmail.com>
2016-09-29 09:56:19 +0200

    aone more

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.send('index.html');
+  res.send(index);
 });
 
 app.listen(PORT, function () {
