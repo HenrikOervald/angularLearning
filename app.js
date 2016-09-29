f07a12bb4a9dcaa5664b1ad6292d649d5823fe5c
HenrikOervald <henrik.oervald@gmail.com>
2016-09-29 09:54:23 +0200

    yet another try

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.render('pages/index.html');
+  res.send('index.html');
 });
 
 app.listen(PORT, function () {
   console.log('Example app listening on port '+ PORT +'!');
-})
+})	
